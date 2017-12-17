9931ca3cee94a3dad94394324e61752433c658ca
loserVEVO <loser@national.shitposting.agency>
2017-12-17 18:10:01 +0300

    good cleanup tenrys

--- lua/mingeban/commands/psa.lua
+++ lua/mingeban/commands/psa.lua
@@ -2,11 +2,12 @@
 if SERVER then
 	util.AddNetworkString("mingeban_command_psa")
 
-	local psa = mingeban.CreateCommand("psa", function(caller, line)
+	local psa = mingeban.CreateCommand("psa", function(caller, line, msg)
 		net.Start("mingeban_command_psa")
 			net.WriteString(msg)
 		net.Broadcast()
 	end)
+	psa:AddArgument(ARGTYPE_STRING)
 	psa:SetHideChat(true)
 end
 
